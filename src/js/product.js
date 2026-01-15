db6dc6c33d1ec1689bc4896cf6593f4d6f8fd088
jmalan210 <>
2026-01-14 17:14:53 -0700

    fixed a few tiny things after lint

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,9 @@
-import { getLocalStorage, setLocalStorage, getParam } from "./utils.mjs";
+import {getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import productDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
-const productId = getParam('product');
+const productId = getParam("product");
 
 const product = new productDetails(productId, dataSource);
 product.init();
