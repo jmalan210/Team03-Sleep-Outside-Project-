ab446550b7fdc699b45530b554cad6aec1e4be0c
LaceySana <laceysanabria@gmail.com>
2026-01-16 12:32:21 -0700

    empty cart error fixed

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,7 +2,7 @@ import { getLocalStorage, getCartCount } from "./utils.mjs";
 
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+  const cartItems = getLocalStorage("so-cart") || [];
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
  
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
