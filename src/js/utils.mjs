f3b5ddd6619bfc86eb38a57e2d7533d03d4c2cd6
jmalan210 <>
2026-01-15 20:53:18 -0700

    added a line to addProductToCart so counter updates immediately when product is added to cart.

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -8,6 +8,7 @@ export function qs(selector, parent = document) {
 // retrieve data from localstorage
 export function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
+  
 }
 // save data to local storage
 export function setLocalStorage(key, data) {
