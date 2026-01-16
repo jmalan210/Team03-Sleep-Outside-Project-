4210cd471b244df9a75088e66df9b5bc068b24f2
jmalan210 <>
2026-01-15 19:16:38 -0700

    commented out unneeded code in main.js

--- src/js/main.js
+++ src/js/main.js
@@ -12,16 +12,16 @@ const productList = new ProductList("Tents", dataSource, element);
 productList.init();
 
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+// function addProductToCart(product) {
+//   setLocalStorage("so-cart", product);
+// }
+// // add to cart button event handler
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// // add listener to Add to Cart button
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click", addToCartHandler);
