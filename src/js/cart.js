aa445faf3786e9c404373a4e5745f5cc80d27075
jmalan210 <>
2026-01-15 19:46:19 -0700

    counting cart items in cart.js

--- src/js/cart.js
+++ src/js/cart.js
@@ -3,6 +3,8 @@ import { getLocalStorage } from "./utils.mjs";
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+  const count = htmlItems.length;
+  console.log(count);
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
 
