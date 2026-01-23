05fb35093c845abf4c4841d5e05ffa9f07dac634
shaniaesguerra <shaniae47@gmail.com>
2026-01-23 14:46:32 -0700

    initital commit - product sort set-up

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -10,7 +10,31 @@ const element = document.querySelector(".product-list");
 
 const productList = new ProductList(category, dataSource, element);
 
-
-
 productList.init();
 loadHeaderFooter();
+
+//Filter Results Functionality
+//Get Buttons:
+const brandNamebtnAZ = document.querySelector("#filter-brandNameBtn-AZ"); //Filter Brand name A-Z button
+const brandNamebtnZA = document.querySelector("#filter-brandNameBtn-ZA"); //Filter Brand name Z-A button
+const pricebtnLH = document.querySelector("filter-priceBtn-LH"); //Filter price Lowest - Highest button
+const pricebtnHL = document.querySelector("filter-priceBtn-LH"); //Filter price Hightest - Lowest button
+
+brandNamebtnAZ.addEventListener("click", () => {
+    //Sort Brand Name from A-Z
+});
+
+brandNamebtnZA.addEventListener("click", () => {
+    //Sort Brand Name from Z-A
+});
+
+pricebtnLH.addEventListener("click", () => {
+    //Sort Price from Lowest - Highest
+});
+
+pricebtnHL.addEventListener("click", () => {
+    //Sort Price from Highest - Lowest
+});
+
+
+
