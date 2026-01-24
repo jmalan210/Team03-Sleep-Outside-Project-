e868e1a4ef51d40c9e1700d6d46670e9f2bfeb44
paulajessica-dev <jessicafldasilva@gmail.com>
2026-01-23 22:33:49 -0300

    alter image

--- src/js/cart.js
+++ src/js/cart.js
@@ -11,7 +11,7 @@ function renderCartContents() {
 
 function cartItemTemplate(item) {
 
-  const image = item.Image;
+  const image = item.Image.PrimarySmall;
   const name = item.NameWithoutBrand;
   const color = item.Colors?.[0]?.ColorName;
   const price = item.FinalPrice;
