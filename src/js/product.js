67c6122203461ba25e8aa6d02a0023098daaca4d
shaniaesguerra <shaniae47@gmail.com>
2026-01-30 11:15:56 -0700

    added alert for adding product to cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,4 @@
-import { getParam, loadHeaderFooter } from "./utils.mjs";
+import { getParam, loadHeaderFooter, alertMessage } from "./utils.mjs";
 import ExternalServices from "./ExternalServices.mjs";
 import productDetails from "./ProductDetails.mjs";
 
@@ -8,4 +8,14 @@ const productId = getParam("product");
 const product = new productDetails(productId, dataSource);
 product.init();
 loadHeaderFooter();
+
+const addToCartBtn = document.querySelector("#addToCart");
+addToCartBtn.addEventListener("click", () => {
+    //remove all previous alerts
+    const alerts = document.querySelectorAll(".alert");
+    alerts.forEach((alert) => document.querySelector("main").removeChild(alert));
+
+    //make alert messages:
+    alertMessage("Added Product to Cart.");
+});
 
