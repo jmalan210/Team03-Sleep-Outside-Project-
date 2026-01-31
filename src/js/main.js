8293f328af9b5ea3a20d76c6327824179bc9178d
shaniaesguerra <shaniae47@gmail.com>
2026-01-31 11:18:04 -0700

    adding modal dialogue on main

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,34 @@
-import { loadHeaderFooter } from "./utils.mjs";
-import ExternalServices from "./ExternalServices.mjs";
-import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter, getLocalStorage, setLocalStorage, alertMessage, removeAllAlerts } from "./utils.mjs";
 
-// const dataSource = new ExternalServices("tents");
-
-// const element = document.querySelector(".product-list");
-
-// const productList = new ProductList("Tents", dataSource, element);
-
-
-
-// productList.init();
 loadHeaderFooter();
 
+const welcomePopUp = document.querySelector(".welcomePopUp");
+const closeBtn = document.querySelector("#closeBtn");
+const couponBtn = document.querySelector("#couponBtn");
 
+//Trigger event: pop-up the modal when all conent has been loaded
+window.addEventListener("DOMContentLoaded", () => {
+    const visited = getLocalStorage("visited");
+    //check if local storage has the variable "visited"
+    setTimeout(() => {
+        if (!visited) {
+            //show welcome pop-up
+            welcomePopUp.showModal();
+            //update local storage to show that user has visited
+            //won't show again on the next load unless "visited" is deleted in localStorage
+            setLocalStorage("visited", true);
+        }
+    },1000);
+})
+
+closeBtn.addEventListener("click", () => {
+    welcomePopUp.close();
+});
+
+couponBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    welcomePopUp.close();
+    removeAllAlerts();
+    alertMessage("Coupon sent to your email");
+    setTimeout(removeAllAlerts,3000);
+})
